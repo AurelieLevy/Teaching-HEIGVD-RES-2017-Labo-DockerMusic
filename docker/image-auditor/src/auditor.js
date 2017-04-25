7d441e23ee67460d1215de068272bcaade0b41ba
Aurelie Levy <aurelie.levy@heig-vd.ch>
2017-04-25 11:44:35 +0200

    musician semble ok

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -10,28 +10,49 @@ var server = net.createServer(function(socket) {
 
 server.listen(1337, '127.0.0.1');*/
 
-var instruments = {
+
+var protocol = require('./protocol');
+//var entendu[];//trace de ce qui est entendu
+
+//il faut les datagrammes des sockets
+var datagrammes = require('dgram');
+var socket = datagrammes.createSocket('udp4'); //comme vu en cours
+
+var currentInstruments = new Map();
+
+/*var instruments = {
   piano: "ti-ta-ti",
   trumper: "pouet",
   flute: "trulu",
   violin: "gzi-gzi",
   drum: "boom-boom"
-}
+}*/
 
-var protocol.require('./protocol');
-var entendu[];//trace de ce qui est entendu
+//ecoute musiciens
+socket.on('listening', function(){
+		var address = socket.address();
+		console.log("Ecoute sur: " = address.address + ":" + address.port);
+	});
+	
+//recup du payload
+socket.on('message', function(message, source){
+		console.log("Message: " + message + " Source port:" + source.port);
+		var currentInstruments = JSON.parse(message);
+		//REMPLIR MAP
+		
+	});
 
 //serveur TCP
-
+/*
 var net = require('net');
 var serveur = net.createServer();
-
-server.on('connection', musiciensActifs);
-server.listen(protocol.PORT_ECOUTE);
-
+serveur.on('connection', musiciensActifs);
+serveur.listen(protocol.PORT_ECOUTE);
+*/
+/*
 function musiciensActifs(client){
 	
 	
-}
+}*/
 
 
--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -7,11 +7,12 @@ const INSTRUMENTS = {
 };
 
 var protocol = require('./protocol');
+
 //il faut les datagrammes des sockets
 var datagrammes = require('dgram');
 var uuid = require('uuid');//global unique identifieur. npm
 
-var socket = dgram.createSocket('udp4'); //comme vu en cours
+var socket = datagrammes.createSocket('udp4'); //comme vu en cours
 
 
 
@@ -21,29 +22,30 @@ function Musicien(instrument){
 	this.instrument = instrument;
 	this.uuid = uuid.v4();
 	this.activeSince = new Date().toString();
+}
+
+Musicien.prototype.update = function(){
+	var toSend = {
+		'uuid' : uuid,
+		'instrument' : this.instrument,
+		'sound' : INSTRUMENTS[process.argv[2]]
+	};
+	var payload = JSON.stringify(toSend);//transforme en json
 	
-	Musicien.prototype.update = function(){
-		var toSend = {
-			uuid : uuid,
-			instrument  : this.instrument
-		};
-		var payload = JSON.stringify(toSend);//transforme en json
-		
-		message = new Buffer(payload);
-		socket.send(message, 0, message.length, protocol.PORT_CONNEXION_MULTICAST, protocol.MULTICAST_ADDRESS,
+	message = new Buffer(payload);
+	socket.send(message, 0, message.length, protocol.PORT_CONNEXION_MULTICAST, protocol.MULTICAST_ADDRESS,
 		function(err, bytes){
 			console.log("envoi du payload: " + payload + " via port " + socket.address().port);
 		});
-	}
 }
 
 
-var instrument = process.argv[2];//lecture de la commande pour obtenir l'instrument souhaite
-if(INSTRUMENTS[instrument] === undefined){//verification de la demande
+var instru = process.argv[2];//lecture de la commande pour obtenir l'instrument souhaite
+if(INSTRUMENTS[instru] === undefined){//verification de la demande
 	process.on('exit', function(){
 		console.log("Instrument inconnu");
 		process.exit(1);
 	});
 }
-
-var mus = new Musicien(instrument);
+var mus = new Musicien(instru);
+setInterval(mus.update,1000);
